d97753a8fe7a4417b4b13a9c33b47efcc4b0fd49
Abhi <92674907+abhig1289@users.noreply.github.com>
2024-09-02 11:47:22 +0530

    Create Anagrams.js

---
+++ Anagrams.js
@@ -0,0 +1,9 @@
+function areAnagrams(str1, str2) {
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+    return sortedStr1 === sortedStr2;
+}
+
+// Example usage:
+console.log(areAnagrams("listen", "silent")); // true
+console.log(areAnagrams("hello", "world")); // false
